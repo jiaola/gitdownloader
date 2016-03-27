40a557f9af50d65f42288e3d0c8e4eee88ca0aca
djiao <djiao@iu.edu>
2016-03-26 22:15:02 -0400

    Full implementation

--- src/main/java/ws/wenku/gitdownloader/App.java
+++ src/main/java/ws/wenku/gitdownloader/App.java
@@ -2,9 +2,12 @@ package ws.wenku.gitdownloader;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,13 +16,31 @@ public class App {
         super();
     }
 
-    public boolean downloadDir(String url) {
-
-        return false;
+    public boolean downloadDir(String url, File dir) {
+        if (!dir.exists()) dir.mkdirs();
+        List<Item> list = listUrl(url);
+        final List<Boolean> result = new ArrayList<>();
+        list.forEach(item -> {
+            if ("dir".equals(item.getType())) { //dir
+                File newDir = new File(dir, item.getName());
+                result.add(downloadDir(item.getUrl(), newDir));
+            } else { // file
+                File file = FileUtils.getFile(dir, item.getName());
+                result.add(downloadFile(item.getDownload_url(), file));
+            }
+        });
+        return !result.contains(Boolean.FALSE);
     }
 
-    public boolean downloadFile(String url) {
-        return false;
+    public boolean downloadFile(String url, File file) {
+        boolean success = true;
+        try {
+            FileUtils.copyURLToFile(new URL(url), file);
+        } catch (IOException ioex) {
+            System.err.println(ioex.getMessage());
+            success = false;
+        }
+        return success;
     }
 
     public List<Item> listUrl(String url) {
@@ -34,13 +55,24 @@ public class App {
         try (Scanner scanner = new Scanner(new URL(url).openStream(), "UTF-8")) {
             out = scanner.useDelimiter("\\A").next();
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.err.println(ex.getMessage());
         }
         return out;
     }
 
 
     public static void main( String[] args ) {
+        if (args.length != 2) {
+            System.err.println("Usage: java ws.wenku.gitdownloader.App <URL> <DIR>");
+            System.exit(1);
+        }
 
+        App app = new App();
+        File dir = new File(args[1]);
+        if (!dir.exists()) dir.mkdirs();
+        boolean result = app.downloadDir(args[0], dir);
+        if (!result) {
+            System.out.println("Completed but there were problems downloading the folder");
+        }
     }
 }
--- src/test/java/ws/wenku/gitdownloader/AppTest.java
+++ src/test/java/ws/wenku/gitdownloader/AppTest.java
@@ -3,14 +3,14 @@ package ws.wenku.gitdownloader;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import java.io.File;
+import java.nio.file.Files;
 import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
@@ -46,5 +46,26 @@ public class AppTest
         List<Item> list = app.listUrl("https://api.github.com/repos/jiaola/gitdownloader/contents/src");
         assertNotNull(list);
         assertEquals(2, list.size());
+    }
+
+    public void testDownloadUrl() throws Exception {
+        App app = new App();
+        String url = "https://raw.githubusercontent.com/jiaola/gitdownloader/master/src/main/java/ws/wenku/gitdownloader/App.java";
+        File tempFile =  File.createTempFile("App", "java");
+        tempFile.deleteOnExit();
+        boolean result = app.downloadFile(url, tempFile);
+        assertTrue(tempFile.length() > 0);
+        assertTrue(result);
+    }
+
+    public void testDownloadDir() throws Exception {
+        App app = new App();
+        String url = "https://api.github.com/repos/jiaola/gitdownloader/contents/src";
+        File tempDir = Files.createTempDirectory("gitdownloader_").toFile();
+        tempDir.deleteOnExit();
+        boolean result = app.downloadDir(url, tempDir);
+        assertTrue(result);
+        File[] dirs = tempDir.listFiles(File::isDirectory);
+        assertEquals(2, dirs.length);
     }
 }
